e2f78661af55cf97ee664cc4ea9a615d762bc4d8
Denperidge <cat@denperidge.com>
2024-12-07 14:56:27 +0100

    Finished implementing eleventy-test

--- test/test-scenarios.js
+++ test/test-scenarios.js
@@ -1,18 +1,20 @@
 const test = require('ava');
 const {cwd} = require('process');
-const { readdirSync } = require('fs');
-const {join} = require("path");
+const {readdirSync} = require('fs');
 const {buildScenarios} = require('eleventy-test');
 
-test("outputDir overrides eleventy output dir", async (t) => {
-    buildScenarios(cwd(), false, 'test/scenarios/', 'test/scenario-input/').then((results) => {
-    
-        console.log(results["3--with-outputDir"]._files)
-        console.log(results["3--without-outputDir"].files)
-        // Has no outputDir defined, defaults to eleventyConfig directory
-        t.is(Object.keys(results["3--without-outputDir"].files).length, 1)
-        // Has an outputDir defined, overrides eleventyConfig directory
-        t.is(Object.keys(results["3--with-outputDir"].files).length, 1)
-        t.is(readdirSync("test/scenarios/3--with-outputDir/overriden-outDir/").length, 6)
-    })
+test('outputDir overrides eleventy output dir', async (t) => {
+  const results = await buildScenarios({
+    projectRoot: cwd(),
+    returnArray: false,
+    scenariosDir: 'test/scenarios/',
+    globalInputDir: 'test/scenario-input/',
+  });
+
+
+  // Has no outputDir defined, defaults to eleventyConfig directory
+  t.is(Object.keys(results['3--without-outputDir'].files).length, 7);
+  // Has an outputDir defined, overrides eleventyConfig directory
+  t.is(Object.keys(results['3--with-outputDir'].files).length, 1);
+  t.is(readdirSync('test/scenarios/3--with-outputDir/overriden-outDir/').length, 6);
 });
